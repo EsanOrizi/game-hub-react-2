8c8587e48c68e058ef6d84ae60fca9bba191cdd8
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2024-04-09 00:58:19 +0100

    Fetching Genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,20 @@
+import genres from "../data/genres";
+import {useQuery} from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import {FetchResponse} from "./useData";
+
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
+}
+
+const useGenres = () => useQuery({
+  queryKey: ['genres'],
+  queryFn: () => apiClient
+      .get<FetchResponse<Genre>>('/genres').then(res => res.data),
+  staleTime: 24 * 60 * 60 * 1000, // 24H
+  initialData: {count: genres.length, results: genres}
+});
+
+export default useGenres;
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "54f97e5d6b004c3686bc549fef5b174a",
+  },
+});
